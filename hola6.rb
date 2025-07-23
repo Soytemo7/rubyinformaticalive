d98aec0c43a9250a5f931fd5a9ba3f95e80405c5
Soytemo7 <contador.manuel0@gmail.com>
2025-07-23 17:19:21 -0600

    Final

--- hola6.rb
+++ hola6.rb
@@ -2,3 +2,16 @@
 # rails new mi_app
 # entrar a la carpeta y "rails server"
 # rails db:create
+# bin/bundle install
+
+# rails generate model Post title:string body:text
+# bin/rails db:migrate
+
+# bin/rails console
+# Post.create(title:"mi primer post",body:"Hola mundo")
+# Post.all
+
+# rails generate controller Posts index
+
+# rails routes
+# 
